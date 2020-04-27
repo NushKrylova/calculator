3b059ee2b2b53fdf7254b5291b4b17779406c2f0
Anna Krylova <annakry@wix.com>
2020-04-27 21:47:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,184 +1,222 @@
 import React from 'react';
 import './App.css';
 
-const regexOperation = /[\+\-\*\/=]/
+const SESSION = "Session"
+const BREAK = "Break"
+const SESSION_LENGTH = "sessionLength"
+const BREAK_LENGTH = "breakLength"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: "0",
-      lastAcceptedValue: "",
-      prevOperation: "",
-      formula: "",
-      result: "",
-      negativeValue: false
+      breakLength: 5,
+      sessionLength: 25,
+      timerLabel: SESSION,
+      minutes: 0,
+      seconds: 0,
+      running: false,
+      intervalId: ""
     };
   }
-  handleClear() {
-    this.setState({
-      display: "0",
-      lastAcceptedValue: "",
-      prevOperation: "",
-      formula: "",
-      result: "",
-    })
-  }
-  handleFirstInput(value) {
+  componentDidMount() {
     this.setState((state) => {
       return {
-        display: value,
-        lastAcceptedValue: value,
-        formula: value,
-        result: value
+        minutes: state.sessionLength
       }
     })
   }
-  handleOperations(value) {
-    let _result = this.calulateResult(this.state.prevOperation, this.state.result, this.state.lastAcceptedValue)
-    if (value === "=") {
-      this.setState((state) => {
-        return {
-          display: _result,
-          prevOperation: "",
-          formula: state.formula + value + _result,
-          result: _result,
-        }
-      })
-    } else if (value === "-" && regexOperation.test(this.state.display)) {
-      this.setState((state) => {
-        return {
-          display: value,
-          formula: state.formula + value,
-          lastAcceptedValue: "",
-          negativeValue: true
-        }
-      })
-    } else {
-      this.setState((state) => {
-        return {
-          display: value,
-          lastAcceptedValue: "",
-          prevOperation: value,
-          formula: state.formula + value,
-          result: _result,
-          negativeValue: false
-        }
-      })
-    }
+  convertToTimeFormat(minutes, seconds) {
+    let sec;
+    seconds < 10 ? sec = "0" + seconds : sec = seconds
+    let min;
+    minutes < 10 ? min = "0" + minutes : min = minutes
+    return min + ":" + sec
   }
-  handleNumber(value) {
-    //clean display from operation
-    if (regexOperation.test(this.state.display)) {
-      this.setState({
-        display: ""
-      })
-    }
-    //no more than one 0 or . can be taken
-    if ((value === "0" && this.state.display === "0") || (value === "." && this.state.display.includes("."))) {
-      return;
-    }
-    //handle the first value
-    if (this.state.prevOperation === "") {
-      this.setState((state) => {
-        return {
-          display: state.display + value,
-          lastAcceptedValue: state.lastAcceptedValue + value,
-          formula: state.formula + value,
-          result: state.result + value
+  reset() {
+    this.clearInterval(this.state.intervalId)
+    this.setState({
+      breakLength: 5,
+      sessionLength: 25,
+      timerLabel: SESSION,
+      minutes: 25,
+      seconds: 0,
+    })
+    this.audioBeep.pause();
+    this.audioBeep.currentTime = 0;
+  }
+  decrement(value) {
+    switch (value) {
+      case BREAK_LENGTH:
+        if (this.state.breakLength === 1) {
+          return
         }
-      })
-    } else {
-      if (this.state.negativeValue) {
         this.setState((state) => {
           return {
-            display: state.display + value,
-            lastAcceptedValue: "-" + value,
-            formula: state.formula + value,
-            negativeValue: false,
+            breakLength: state.breakLength - 1
           }
-        })
-      } else {
+        });
+        break;
+      case SESSION_LENGTH:
+        if (this.state.sessionLength === 1) {
+          return
+        }
         this.setState((state) => {
           return {
-            display: state.display + value,
-            lastAcceptedValue: state.lastAcceptedValue + value,
-            formula: state.formula + value,
+            sessionLength: state.sessionLength - 1,
+            minutes: state.sessionLength - 1,
           }
         })
-      }
+        break;
+    }
+
+  }
+  increment(value) {
+    switch (value) {
+      case BREAK_LENGTH:
+        if (this.state.breakLength === 60) {
+          return
+        }
+        this.setState((state) => {
+          return {
+            breakLength: state.breakLength + 1
+          }
+        })
+        break
+      case SESSION_LENGTH:
+        if (this.state.sessionLength === 60) {
+          return
+        }
+        this.setState((state) => {
+          return {
+            sessionLength: state.sessionLength + 1,
+            minutes: state.sessionLength + 1,
+          }
+        })
+        break
     }
   }
-  handleInput(e) {
-    let value = e.target.value
-    //handle default state 0
-    if (this.state.formula === "") {
-      this.handleFirstInput(value)
-    } else {
-      //operation entered
-      if (regexOperation.test(value)) {
-        this.handleOperations(value)
-      }
-      //number is enetered 
-      else {
-        this.handleNumber(value)
-      }
-    }
-  }
-  calulateResult(operation, a, b) {
-    console.log("operation:", operation, " a:", a, " b:", b)
-    //if number was accepted and operation wasn't entered
-    if (this.state.prevOperation === "" || regexOperation.test(this.state.display)) {
-      return a
-    }
-    switch (operation) {
-      case "+": return parseFloat(a) + parseFloat(b);
-      case "-": return parseFloat(a) - parseFloat(b);
-      case "*": return parseFloat(a) * parseFloat(b);
-      case "/": return (Math.round((parseFloat(a) / parseFloat(b)) * 10000)) / 10000;
-    }
+  clearInterval() {
+    clearInterval(this.state.intervalId);
+    this.setState({
+      intervalId: "",
+      running: false
+    })
   }
 
+  startStop() {
+    if (this.state.running) {
+      this.clearInterval(this.state.intervalId)
+    } else {
+      let intervalId = setInterval(() => this.countDown(), 1000);
+      this.setState((state) => {
+        return {
+          intervalId: intervalId,
+          running: true,
+          // minutes:0,
+          // seconds:5
+        }
+      })
+    }
+  }
+  switchTimer() {
+    if (this.state.timerLabel === SESSION) {
+      this.setState((state) => {
+        return {
+          timerLabel: BREAK,
+          minutes: state.breakLength,
+        }
+      })
+    } else {
+      this.setState((state) => {
+        return {
+          timerLabel: SESSION,
+          minutes: state.sessionLength,
+        }
+      })
+    }
+  }
+  countDown() {
+    //play sound, start at 00:01
+    if (this.state.minutes === 0 && this.state.seconds === 1) {
+      this.playSound()
+    }
+    //handle 00:00
+    if (this.state.minutes === 0 && this.state.seconds === 0) {
+      this.clearInterval(this.state.intervalId)
+      this.switchTimer()
+      this.startStop()
+      return
+    }
+    //handle -1s
+    if (this.state.seconds === 0) {
+      this.setState((state) => {
+        return {
+          minutes: state.minutes - 1,
+          seconds: 59,
+        }
+      })
+    } else {
+      this.setState((state) => {
+        return {
+          seconds: state.seconds - 1,
+        }
+      })
+    }
+  }
+  playSound() {
+    this.audioBeep.currentTime = 0;
+    this.audioBeep.play();
+
+  }
   render() {
     return (
-      <div id="calculator">
+      <div id="pomodoro" className="title">Pomodoro Clock
         <div className="container">
-          <div className="containerDisplay">
-            <div id="formula" className="formula display">{this.state.formula}</div>
-            <div id="display" className="input display">{this.state.display}</div>
+          <div className="length">
+            <div className="length-block">
+              <div id="break-label" className="text">Break Length</div>
+              <div className="buttons">
+                <button id="break-decrement" className="arrow-button" onClick={this.decrement.bind(this, BREAK_LENGTH)}>
+                  <i className="fa fa-arrow-down fa-2x"></i>
+                </button>
+                <div id="break-length" className="text">{this.state.breakLength}</div>
+                <button id="break-increment" className="arrow-button" onClick={this.increment.bind(this, BREAK_LENGTH)}>
+                  <i className="fa fa-arrow-up fa-2x"></i>
+                </button>
+              </div>
+            </div>
+            <div className="length-block">
+              <div id="session-label" className="text">Session Length</div>
+              <div className="buttons">
+                <button id="session-decrement" className="arrow-button" onClick={this.decrement.bind(this, SESSION_LENGTH)}>
+                  <i className="fa fa-arrow-down fa-2x"></i>
+                </button>
+                <div id="session-length" className="text">{this.state.sessionLength}</div>
+                <button id="session-increment" className="arrow-button" onClick={this.increment.bind(this, SESSION_LENGTH)}>
+                  <i className="fa fa-arrow-up fa-2x"></i>
+                </button>
+              </div>
+            </div>
           </div>
-          <Buttons clear={this.handleClear.bind(this)} input={this.handleInput.bind(this)} />
-        </div >
-      </div>
+          <div className="session">
+            <div id="timer-label">{this.state.timerLabel}</div>
+            <div id="time-left">{this.convertToTimeFormat(this.state.minutes, this.state.seconds)}</div>
+          </div>
+          <div className="control-buttons buttons">
+            <button id="start_stop" className="arrow-button" onClick={this.startStop.bind(this)}>
+              <i className="fa fa-play fa-2x"></i>
+              <i className="fa fa-pause fa-2x"></i>
+            </button>
+            <button id="reset" className="arrow-button" onClick={this.reset.bind(this)}>
+              <i className="fa fa-refresh fa-2x"></i>
+            </button>
+          </div>
+          <audio id="beep" src="https://goo.gl/65cBl1" ref={(audio) => { this.audioBeep = audio; }} ></audio>
+        </div>
+      </div >
     );
   }
 }
-
-class Buttons extends React.Component {
-  render() {
-    return (
-      <div>
-        <button id="clear" className="button clear" onClick={this.props.clear}>AC</button>
-        <button id="divide" className="button operations" onClick={this.props.input} value="/">/</button>
-        <button id="multiply" className="button operations" onClick={this.props.input} value="*">*</button>
-        <button id="seven" className="button" onClick={this.props.input} value="7">7</button>
-        <button id="eight" className="button" onClick={this.props.input} value="8">8</button>
-        <button id="nine" className="button" onClick={this.props.input} value="9">9</button>
-        <button id="subtract" className="button operations" onClick={this.props.input} value="-">-</button>
-        <button id="four" className="button" onClick={this.props.input} value="4">4</button>
-        <button id="five" className="button" onClick={this.props.input} value="5">5</button>
-        <button id="six" className="button" onClick={this.props.input} value="6">6</button>
-        <button id="add" className="button operations" onClick={this.props.input} value="+">+</button>
-        <button id="one" className="button" onClick={this.props.input} value="1">1</button>
-        <button id="two" className="button" onClick={this.props.input} value="2">2</button>
-        <button id="three" className="button" onClick={this.props.input} value="3">3</button>
-        <button id="equals" className="button" onClick={this.props.input} value="=">=</button>
-        <button id="zero" className="button" onClick={this.props.input} value="0">0</button>
-        <button id="decimal" className="button" onClick={this.props.input} value=".">.</button>
-      </div>
-    );
-  }
-}
-
 export default App;
 
